e236e427e92cf6b2faf19e563c4340b196e43af4
Aristarkh Zagorodnikov <xm@drive.net>
2021-12-22 13:01:10 +0300

    Modernize sources

--- httploadgen/Program.cs
+++ httploadgen/Program.cs
@@ -4,73 +4,72 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HttpLoadGen
+namespace HttpLoadGen;
+
+public static class Program
 {
-    public static class Program
+    public static void Main()
     {
-        public static void Main()
+        const uint Requests = 800000;
+        const uint Parallelism = 8;
+        const ushort Concurrency = 6;
+        var uri = new Uri("http://localhost:5002/v0.1/locate", UriKind.Absolute);
+        using var client = new HttpClient();
+        Run(client, uri, Concurrency, Parallelism, Concurrency);
+        GC.Collect();
+        var perThreadRequests = (Requests + Parallelism - 1) / Parallelism;
+        var st = Stopwatch.StartNew();
+        Run(client, uri, perThreadRequests, Parallelism, Concurrency);
+        Console.WriteLine(Math.Floor(perThreadRequests * Parallelism / st.Elapsed.TotalSeconds));
+    }
+
+    private static void Run(HttpClient client, Uri uri, uint requests, uint parallelism, ushort concurrency)
+    {
+        var tasks = new Task[parallelism];
+        for (var i = 0; i < parallelism; i++)
         {
-            const uint Requests = 800000;
-            const uint Parallelism = 8;
-            const ushort Concurrency = 6;
-            var uri = new Uri("http://localhost:5002/v0.1/locate", UriKind.Absolute);
-            using var client = new HttpClient();
-            Run(client, uri, Concurrency, Parallelism, Concurrency);
-            GC.Collect();
-            var perThreadRequests = (Requests + Parallelism - 1) / Parallelism;
-            var st = Stopwatch.StartNew();
-            Run(client, uri, perThreadRequests, Parallelism, Concurrency);
-            Console.WriteLine(Math.Floor(perThreadRequests * Parallelism / st.Elapsed.TotalSeconds));
+            tasks[i] = Task.Run(() => Run(client, uri, requests, concurrency));
         }
 
-        private static void Run(HttpClient client, Uri uri, uint requests, uint parallelism, ushort concurrency)
-        {
-            var tasks = new Task[parallelism];
-            for (var i = 0; i < parallelism; i++)
-            {
-                tasks[i] = Task.Run(() => Run(client, uri, requests, concurrency));
-            }
+        Task.WaitAll(tasks);
+    }
 
-            Task.WaitAll(tasks);
+    private static void Run(HttpClient client, Uri uri, uint requests, ushort concurrency)
+    {
+        if (concurrency < 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(concurrency), concurrency, "Concurrency is less than 1.");
         }
 
-        private static void Run(HttpClient client, Uri uri, uint requests, ushort concurrency)
+        if (requests < concurrency)
         {
-            if (concurrency < 1)
-            {
-                throw new ArgumentOutOfRangeException(nameof(concurrency), concurrency, "Concurrency is less than 1.");
-            }
+            throw new ArgumentOutOfRangeException(nameof(requests), requests, "Number of requests is less than concurrency.");
+        }
 
-            if (requests < concurrency)
-            {
-                throw new ArgumentOutOfRangeException(nameof(requests), requests, "Number of requests is less than concurrency.");
-            }
-
-            using var content = new StringContent("8.8.8.8", Encoding.ASCII);
-            var tasks = new Task<HttpResponseMessage>[concurrency];
-            for (var i = 0; i < requests; i++)
-            {
+        using var content = new StringContent("8.8.8.8", Encoding.ASCII);
+        var tasks = new Task<HttpResponseMessage>[concurrency];
+        for (var i = 0; i < requests; i++)
+        {
 #pragma warning disable CA2000 // Dispose objects before losing scope -- HttpRequestMessage only disposes content and we would like to share it
-                var requestMessage = new HttpRequestMessage(HttpMethod.Post, uri) { Content = content };
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, uri) { Content = content };
 #pragma warning restore CA2000 // Dispose objects before losing scope
-                int taskIndex;
-                if (i >= concurrency)
-                {
-                    taskIndex = Task.WaitAny(tasks);
-                    tasks[taskIndex].Result.EnsureSuccessStatusCode();
-                }
-                else
-                {
-                    taskIndex = i;
-                }
-
-                tasks[taskIndex] = client.SendAsync(requestMessage);
-            }
-
-            foreach (var task in tasks)
+            int taskIndex;
+            if (i >= concurrency)
             {
-                task.Result.EnsureSuccessStatusCode();
+                taskIndex = Task.WaitAny(tasks);
+                tasks[taskIndex].Result.EnsureSuccessStatusCode();
             }
+            else
+            {
+                taskIndex = i;
+            }
+
+            tasks[taskIndex] = client.SendAsync(requestMessage);
+        }
+
+        foreach (var task in tasks)
+        {
+            task.Result.EnsureSuccessStatusCode();
         }
     }
 }
