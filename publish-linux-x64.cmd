eb9fadb4a80b2d8f24fad129d832ceb69859f9e9
Aristarkh Zagorodnikov <xm@x-infinity.com>
2022-07-15 11:50:23 +0300

    Refresh project-system

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -1,5 +1,5 @@
 @echo off
 rmdir /s /q packages\linux-x64\httploadgen
 mkdir packages\linux-x64\httploadgen
-dotnet publish httploadgen --force --output packages\linux-x64\httploadgen -c Integration -r linux-x64 --self-contained false
+dotnet publish httploadgen --force --output packages\linux-x64\httploadgen -c Integration -r linux-x64 --no-self-contained
 del packages\linux-x64\httploadgen\*.deps.json
